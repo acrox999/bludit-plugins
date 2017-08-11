152a5327fdf1685adc81006adb6cbb197978c1e7
Hakim Zulkufli <acrox999@gmail.com>
2017-08-11 20:10:22 +0800

    Some basic configuration options for woofmark.

--- woofmark/js/load.js
+++ woofmark/js/load.js
@@ -1,9 +1,3 @@
-// Create a woofmark instance
-var wm = woofmark(document.querySelector('#jscontent'), {
-  parseMarkdown: megamark,
-  parseHTML: domador
-});
-
 // Newline fix for WYSIWYG mode -- http://wadmiraal.net/lore/2012/06/14/contenteditable-hacks-returning-like-a-pro/
 $("div.wk-wysiwyg").keydown(function(e) {
 	if(e.which === 13) {
@@ -55,21 +49,80 @@ $("div.wk-wysiwyg").keydown(function(e) {
 	}
 });
 
+/* var debug=1;
+$("div.wk-wysiwyg").keydown(function(e) {
+	if(e.which === 13) {
+		$(".wk-wysiwyg").on('blur keyup paste focus focusout',function (){
+			if($(".wk-wysiwyg").children().length==0)
+			{
+				if(debug) console.log("add p");
+				//Only Test now
+				var text=$(".wk-wysiwyg").text();
+				$(".wk-wysiwyg").empty();
+				$(".wk-wysiwyg").append("<p>"+text+"</p>");
+			}
+			
+			if(debug) console.log("-------------------------");
+			if(debug) console.log($(".wk-wysiwyg").children());
+			for(var i=0;i<$(".wk-wysiwyg").children().length;i++)
+			{
+				var tag=$(".wk-wysiwyg").children().eq(i).prop("tagName").toLowerCase();
+				console.log("i="+i+" ["+tag+"]");
+				if(i%3==2)
+				{
+					if($(".wk-wysiwyg").children().length==i+1 && $(".wk-wysiwyg").children().last().text()=="")
+						continue;
+					
+					if(tag!="hr")
+					{
+						if(debug) console.log("  add hr");
+						$(".wk-wysiwyg").children().eq(i).before("<hr/>")    
+					}
+				}
+				else if(tag=="hr")
+				{
+					if(debug) console.log("  rm hr");
+					$(".wk-wysiwyg").children().get(i).remove();
+				}
+				else if(tag=="br")
+				{
+					if(debug) console.log("  br");
+					$(".wk-wysiwyg").children().eq(i).remove();
+					var text=$(".wk-wysiwyg").children().length>=i?$(".wk-wysiwyg").children().eq(i+1).text():"";
+					$(".wk-wysiwyg").children().eq(i).after($("<p>"+text +"</p>"));
+					$(".wk-wysiwyg").children().eq(i).append("<p>"+text+"</p>");
+					if($(".wk-wysiwyg").children().length>=i+1) 
+						$(".wk-wysiwyg").children().eq(i+1).remove();    
+				}
+				else if (tag!="p")
+				{
+					if(debug) console.log("  not p");
+					var text=$(".wk-wysiwyg").children().eq(i).text();
+					$(".wk-wysiwyg").children().eq(i).remove();
+					$(".wk-wysiwyg").children().eq(i).after($("<p>"+text +"</p>"));
+				}
+				
+			}
+			
+		});
+	}
+}); */
 // WYSIWYG save fix for Bludit
 $("button").on('click', function() {
 	if( $(this).html() === "Save" ) {
 		
 		// Get content from WYSIWYG editor
-		var content = wm.editable.innerHTML;
+		//var content = wm.editable.innerHTML;
+		var content = wm.value(); // Save in Markdown.
 
 		// Convert to Markdown for a cleaner base
 		//var content = wm.parseHTML();
 		
 		// Trim the content
-		content = (content.trim) ? content.trim() : content.replace(/^\s+/,'');
+		//content = (content.trim) ? content.trim() : content.replace(/^\s+/,'');
 		
 		// Give the content from WYSIWYG (if needed) to textarea so that Bludit could catch it
-		if( content != '' && !($("div.wk-wysiwyg").hasClass("wk-hide")) ) {
+		if( !($("div.wk-wysiwyg").hasClass("wk-hide")) ) {
 			$("textarea#jscontent").html(content);
 		}
 	}
--- woofmark/plugin.php
+++ woofmark/plugin.php
@@ -8,6 +8,33 @@ class pluginWoofmark extends Plugin {
 		'edit-post',
 		'edit-page'
 	);
+	
+	public function init() {
+		$this->dbFields = array(
+			'defaultMode'=>'markdown',
+			'modeMarkdown'=>'true',
+			'modeHTML'=>'true',
+			'modeWYSIWYG'=>'true',
+			'rememberMode'=>'false'
+		);
+	}
+	
+	public function form() {
+		global $Language;
+		
+		$html  = '';
+		
+		$html  = '<div>';
+		$html .= '<label>'.$Language->get('Default Mode').'</label>';
+		$html .= '<select name="defaultMode">';
+		$html .= '<option value="markdown" '.($this->getDbField('defaultMode')==='markdown'?'selected':'').'>Markdown</option>';
+		$html .= '<option value="html" '.($this->getDbField('defaultMode')==='html'?'selected':'').'>HTML</option>';
+		$html .= '<option value="wysiwyg" '.($this->getDbField('defaultMode')==='wysiwyg'?'selected':'').'>WYSIWYG</option>';
+		$html .= '</select>';
+		$html .= '</div>';
+		
+		return $html;
+	}
 
 	public function adminHead()
 	{
@@ -43,6 +70,17 @@ class pluginWoofmark extends Plugin {
 			$html .= '<script src="'.$pluginPath.'js/domador.min.js" ></script>';
 			$html .= '<script src="'.$pluginPath.'js/megamark.min.js" ></script>';
 			$html .= '<script src="'.$pluginPath.'js/woofmark.min.js" ></script>';
+			
+			// Create a woofmark instance
+			$html .= '<script>'.PHP_EOL;
+			$html .= 'var wm = woofmark(document.querySelector("#jscontent"), {';
+			$html .= '	parseMarkdown: megamark,';
+			$html .= '	parseHTML: domador,';
+			$html .= '	defaultMode: "'.$this->getDbField('defaultMode').'",';
+			$html .= '	storage: '.$this->getDbField('rememberMode').'';
+			$html .= '});';
+			$html .= '</script>'.PHP_EOL;
+			
 			$html .= '<script src="'.$pluginPath.'js/load.js" ></script>';
 			
 			return $html;
